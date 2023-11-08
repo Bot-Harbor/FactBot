4ac3fdaa2f4bed01cb258f70c63936afdef8adaa
Kaleb Garrett <kalebagarrett@gmail.com>
2023-11-07 21:30:51 -0500

    Added ping command

---
+++ FactBot.App/Slash Commands/Ping.cs
@@ -0,0 +1,22 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+
+namespace FactBot.App.Slash_Commands;
+
+public class Ping : ApplicationCommandModule
+{
+    [SlashCommand("ping", "Will pong back to server.")]
+    public async Task PingCommandAsync(InteractionContext ctx)
+    {
+        var pingEmbed = new DiscordEmbedBuilder()
+        {
+            Title = "Ping Command",
+            Description = "Pong 🏓",
+            Color = DiscordColor.Orange
+        };
+        
+        await ctx.CreateResponseAsync(InteractionResponseType.ChannelMessageWithSource, new DiscordInteractionResponseBuilder()
+            .AddEmbed(pingEmbed));
+    }
+}
