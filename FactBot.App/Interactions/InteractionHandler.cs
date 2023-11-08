974ac134393f86d6a786ff98ae0886c73204c333
Kaleb Garrett <kalebagarrett@gmail.com>
2023-11-07 21:50:28 -0500

    Added interaction handler

---
+++ FactBot.App/Interactions/InteractionHandler.cs
@@ -0,0 +1,19 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using DSharpPlus.EventArgs;
+using FactBot.App.Interfaces;
+
+namespace FactBot.App.Interactions;
+
+public class InteractionHandler
+{
+    public Optional<DiscordColor> ExecuteColorEmbed(IColor command)
+    {
+        return command.Execute();
+    }
+    
+    public Task ExecuteButton(IButton button, DiscordClient sender, ComponentInteractionCreateEventArgs e)
+    {
+        return button.Execute(sender, e);
+    }
+}
