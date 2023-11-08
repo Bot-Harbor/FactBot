751317d39a85575290481ac273f64f8a52e68191
Kaleb Garrett <kalebagarrett@gmail.com>
2023-11-07 21:32:29 -0500

    Added button interface

---
+++ FactBot.App/Interfaces/IButton.cs
@@ -0,0 +1,9 @@
+﻿using DSharpPlus;
+using DSharpPlus.EventArgs;
+
+namespace FactBot.App.Interfaces;
+
+public interface IButton
+{
+    public Task ButtonHandler(DiscordClient sender, ComponentInteractionCreateEventArgs e);
+}
